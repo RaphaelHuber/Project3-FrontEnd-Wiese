98c976c80390d6a8154579a769f07f83e2d668d9
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-27 17:57:44 -0300

    bootstrap implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar.js';
 
 // import ProjectList from './components/projects/ProjectList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>WAAAAAAbbbbbHHHHHHHHHffffffffffff</h1>
-      </div>
+      <Navbar />
     );
   }
 }
