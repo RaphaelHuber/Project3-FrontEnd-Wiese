ac58ebcf961c4790efab16cd8e7457d037db36a1
Maryssael <guilherme.maryssael@gmail.com>
2019-02-27 17:08:24 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+// import ProjectList from './components/projects/ProjectList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>WAAAAAAbbbbbHHHHHHHHHffffffffffff</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
