8a19b0498f2448165de6138260cfae4cb03874c4
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-27 18:11:50 -0300

    implemented navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,23 +5,17 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-boots
 const navbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Wiese</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#home">About Us</Nav.Link>
+          <Nav.Link href="#link">Projects</Nav.Link>
+          <Nav.Link href="#link">My Investments</Nav.Link>
+          <Nav.Link href="#link">My Projects</Nav.Link>
         </Nav>
         <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
+          <Button variant="outline-success">Login</Button>
         </Form>
       </Navbar.Collapse>
     </Navbar>
