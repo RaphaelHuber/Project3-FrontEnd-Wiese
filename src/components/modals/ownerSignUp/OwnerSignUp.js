a2ec67b53adc5465a7be5ee1c4cd8c44b1e01135
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-28 17:13:17 -0300

    implemented modal for signups

---
+++ src/components/modals/ownerSignUp/OwnerSignUp.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import './OwnerSignUp.css';
+
+class OwnerSignUp extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <div className="marginRight1">
+        <Button variant="primary" onClick={this.handleShow}>Create a Project</Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sign Up</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Name</Modal.Body>
+          <Modal.Body>Surname</Modal.Body>
+          <Modal.Body>Company</Modal.Body>
+          <Modal.Body>Password</Modal.Body>
+          <Modal.Body>Repeat Password</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Send
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default OwnerSignUp;
