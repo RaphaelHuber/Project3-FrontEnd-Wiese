b1571d370300b5fe6b025008423d1058fd369a36
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-28 14:46:46 -0300

    added buttons and energy symbols to header

--- src/components/energySymbols/EnergySymbols.js
+++ src/components/energySymbols/EnergySymbols.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Navbar, Nav, Form, Button } from 'react-bootstrap';
 import './EnergySymbols.css';
+import windPowerLight from '../../../public/img/icons/windPowerLight.png';
+import solarLight from '../../../public/img/icons/SolarLight.png';
+import hydroLight from '../../../public/img/icons/HydroLight.png';
+import biomassLight from '../../../public/img/icons/BiomassLight.png';
+import windPowerDark from '../../../public/img/icons/windPowerDark.png';
+import solarDark from '../../../public/img/icons/SolarDark.png';
+import hydroDark from '../../../public/img/icons/HydroDark.png';
+import biomassDark from '../../../public/img/icons/BiomassDark.png';
 
 const energySymbols = () => {
   return (
-    <div className="headerCenterContainer">
-      <h1> Symbols </h1>
+    <div className="headerCenterContainer center">
+      <img className="mainIcons" src={ windPowerDark } />
+      <img className="mainIcons" src={ solarDark } />
+      <img className="mainIcons" src={ hydroDark } />
+      <img className="mainIcons" src={ biomassDark } />
     </div>
   )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navebar from '../navbar/Navbar.js'
 import './Header.css';
-import energySymbols from '../energySymbols/EnergySymbols.js'
+import EnergySymbols from '../energySymbols/EnergySymbols.js'
+import { Button } from 'react-bootstrap';
 
 const header = () => {
   return (
     <div className="header">
       <Navebar />
-      <energySymbols />
+      <div className="containerCol">
+        <EnergySymbols />
+        <div className="containerRow">
+        <Button className="marginLeft1" color="success">success</Button>
+        <Button className="marginRight1" color="success">success</Button>
+        </div>
+      </div>
     </div>
   )
 }
