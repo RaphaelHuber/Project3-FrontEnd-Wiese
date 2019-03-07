091bd917676eed536e7a4606cd1a0ac92c794e39
Raphael Huber <raphael.ehuber@gmail.com>
2019-03-07 17:35:01 -0300

    minor changes to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,21 +8,21 @@ const Footer = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <ul className="styleNone margin1">
-            <li className="font1">About Us</li>
-            <li><Nav.Link href="#whoAreWe">Who are we</Nav.Link></li>
-            <li><Nav.Link href="#ourMission">Our mission</Nav.Link></li>
-          </ul>
-          <ul className="styleNone margin1">
-            <li className="font1">Investors</li>
-            <li><Nav.Link href="#gettingStarted">Getting started</Nav.Link></li>
-            <li><Nav.Link href="#risks">Risks</Nav.Link></li>
-          </ul>
-          <ul className="styleNone margin1">
-            <li className="font1">Project owners</li>
-            <li><Nav.Link href="#raisingFunds">Raising funds</Nav.Link></li>
-            <li><Nav.Link href="#opportunities">Opportunities</Nav.Link></li>
-          </ul>
+          <div className="margin1 footerCol">
+            <p className="font1">About Us</p>
+            <Nav.Link className="padding1" href="#whoAreWe">Who are we</Nav.Link>
+            <Nav.Link className="padding1" href="#ourMission">Our mission</Nav.Link>
+          </div>
+          <div className="margin1 footerCol">
+            <p className="font1">Investors</p>
+            <Nav.Link className="padding1" href="#gettingStarted">Getting started</Nav.Link>
+            <Nav.Link className="padding1" href="#risks">Risks</Nav.Link>
+          </div>
+          <div className="margin1 footerCol">
+            <p className="font1">Project owners</p>
+            <Nav.Link className="padding1" href="#raisingFunds">Raising funds</Nav.Link>
+            <Nav.Link className="padding1" href="#opportunities">Opportunities</Nav.Link>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
