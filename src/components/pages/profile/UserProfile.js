e17073323eb9b837c376724368b07305639b4a9d
Maryssael <guilherme.maryssael@gmail.com>
2019-03-11 17:54:29 -0300

    userprofile props fix

--- src/components/pages/profile/UserProfile.js
+++ src/components/pages/profile/UserProfile.js
@@ -1,57 +1,25 @@
 import React, { Component } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Button, Card, Navbar, Nav, Form, NavDropdown } from 'react-bootstrap';
 import './UserProfile.css';
 
 class UserProfile extends Component {
   constructor(props) {
     super(props);
   }
-
   
   render() {
-    this.props.fetchUser();
-    console.log(this.props.userInSession);
+    let userHTML;
+
+    if(this.props.userInSession) {
+      userHTML = ( <div>
+        <h1>{this.props.userInSession.username}</h1>
+      </div>)
+    } else {
+      userHTML = <h1>Loading</h1>
+    }
 
     return (
-      <Card className="userProfile-size">
-        <Card.Header className="containerRow">
-          <div className="containerCol userProfile-margin1">
-            <div>
-              Target Sum:
-            </div>
-            <div className="center">
-              200.000$
-            </div>
-          </div>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>{username}</Card.Title>
-          <Card.Text className="containerRow">
-            <div>
-              Return on Capital:
-            </div>  
-            <div className="userProfile-marginNum">
-              5%
-            </div>  
-          </Card.Text>
-          <Card.Text className="containerRow">
-            <div>
-              Periodicity:
-            </div>  
-            <div className="userProfile-marginNum">
-              semi-anual
-            </div>  
-          </Card.Text>
-          <Card.Text className="containerRow">
-            <div>
-              Duration:
-            </div>  
-            <div className="userProfile-marginNum">
-              5 years
-            </div>  
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      userHTML
     );
   }
 }
