ccfb0239dbe1fd9466c41b11aaf93202563cc6be
Raphael Huber <raphael.ehuber@gmail.com>
2019-03-12 16:02:08 -0300

    inversed arrows filter

--- src/components/filter/arrowSortPair/ArrowSortPair.js
+++ src/components/filter/arrowSortPair/ArrowSortPair.js
@@ -11,10 +11,8 @@ class ArrowSortPair extends Component {
     return (
       <div className="containerRow">
         <div className="arrowSortPair-margin1">{this.props.criteriaDisplay}</div>
-        
-        <Nav.Link className="arrowSortPair-margin1 noPadding" onClick={this.props.handleUp}><img className="arrowSortPair-size" src='../../../public/img/icons/upArrow.png' name={this.props.criteria}/></Nav.Link>
-        
         <Nav.Link className="arrowSortPair-margin1 noPadding" onClick={this.props.handleDown}><img className="arrowSortPair-size" src='../../../public/img/icons/downArrow.png' name={this.props.criteria}/></Nav.Link> 
+        <Nav.Link className="arrowSortPair-margin1 noPadding" onClick={this.props.handleUp}><img className="arrowSortPair-size" src='../../../public/img/icons/upArrow.png' name={this.props.criteria}/></Nav.Link>
       </div>
     )
   }
